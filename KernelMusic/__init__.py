0fcf72ebe252ef55690913f98a67318e92182a9e
Ekankth <ekankth@gmail.com>
2023-04-23 04:56:25 +0530

    KernelMusic

---
+++ KernelMusic/__init__.py
@@ -0,0 +1,40 @@
+
+
+from KernelMusic.core.bot import KernelBot
+from KernelMusic.core.dir import dirr
+from KernelMusic.core.git import git
+from KernelMusic.core.userbot import Userbot
+from KernelMusic.misc import dbb, heroku, sudo
+
+from .logging import LOGGER
+
+# Directories
+dirr()
+
+# Check Git Updates
+git()
+
+# Initialize Memory DB
+dbb()
+
+# Heroku APP
+heroku()
+
+# Load Sudo Users from DB
+sudo()
+
+# Bot Client
+app = KernelBot()
+
+# Assistant Client
+userbot = Userbot()
+
+from .platforms import *
+
+YouTube = YouTubeAPI()
+Carbon = CarbonAPI()
+Spotify = SpotifyAPI()
+Apple = AppleAPI()
+Resso = RessoAPI()
+SoundCloud = SoundAPI()
+Telegram = TeleAPI()
